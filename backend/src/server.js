d8aa0b19072438eecdd8aac49f57f4198bd6b837
Gopal das <164305686+dasgopal25@users.noreply.github.com>
2026-02-11 14:39:53 +0000

    change

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,10 +19,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
-}));
+app.use(cors());
 
 app.use("/api/user", userRoutes);
 app.use("/api/provider", providerRoutes);
@@ -40,6 +37,6 @@ RedisClient.connect()
   .then(() => console.log("Redis Ready"))
   .catch((err) => console.log("Redis Connect Error:", err));
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 4000, () => {
   console.log("Server running on port " + process.env.PORT);
 });
